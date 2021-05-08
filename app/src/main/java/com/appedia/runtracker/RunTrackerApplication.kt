40e92709d5dc5bb18eb6ef3212ea1becf8382562
PratikBanodkar <pratikbanodkar@gmail.com>
2021-05-08 23:10:31 +0530

    Create Dagger module class, Application class and use App in manifest

---
+++ app/src/main/java/com/appedia/runtracker/RunTrackerApplication.kt
@@ -0,0 +1,8 @@
+package com.appedia.runtracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RunTrackerApplication : Application() {
+}
---
+++ app/src/main/java/com/appedia/runtracker/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.appedia.runtracker.util
+
+object Constants {
+
+    const val RUN_TRACKER_DATABASE_NAME = "run_tracker_db"
+
+}
