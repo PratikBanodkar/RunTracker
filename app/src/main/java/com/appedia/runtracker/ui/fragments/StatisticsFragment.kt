1b8f47ae8c016099f4355c91ca27daf54dc0a4aa
PratikBanodkar <pratikbanodkar@gmail.com>
2021-05-30 22:11:53 +0530

    Statistics fragment implementation. show basic stats

--- app/src/main/java/com/appedia/runtracker/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/appedia/runtracker/ui/fragments/StatisticsFragment.kt
@@ -1,13 +1,62 @@
 package com.appedia.runtracker.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.appedia.runtracker.R
+import com.appedia.runtracker.databinding.FragmentStatisticsBinding
 import com.appedia.runtracker.ui.viewmodels.StatisticsViewModel
+import com.appedia.runtracker.util.Constants.getFormattedCaloriesForUI
+import com.appedia.runtracker.util.Constants.getFormattedDistanceForUI
+import com.appedia.runtracker.util.Constants.getFormattedRunTimeForUI
+import com.appedia.runtracker.util.Constants.getFormattedSpeedForUI
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
+class StatisticsFragment : Fragment() {
 
-    private val viewModel : StatisticsViewModel by viewModels()
+    private lateinit var binding: FragmentStatisticsBinding
+    private val viewModel: StatisticsViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentStatisticsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModelData()
+    }
+
+    private fun observeViewModelData() {
+        viewModel.totalRunTime.observe(viewLifecycleOwner, {
+            it?.let {
+                binding.textViewTotalRunTimeValue.text = getFormattedRunTimeForUI(it)
+            }
+        })
+
+        viewModel.totalDistanceRun.observe(viewLifecycleOwner, {
+            it?.let {
+                binding.textViewTotalDistanceRunValue.text = getFormattedDistanceForUI(it)
+            }
+        })
+
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner, {
+            it?.let {
+                binding.textViewTotalCaloriesBurnedValue.text = getFormattedCaloriesForUI(it)
+            }
+        })
+
+        viewModel.averageSpeed.observe(viewLifecycleOwner, {
+            it?.let {
+                binding.textViewAverageSpeedValue.text = getFormattedSpeedForUI(it)
+            }
+        })
+    }
 }
--- app/src/main/java/com/appedia/runtracker/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/appedia/runtracker/ui/viewmodels/StatisticsViewModel.kt
@@ -1,5 +1,6 @@
 package com.appedia.runtracker.ui.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.appedia.runtracker.data.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -7,9 +8,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class StatisticsViewModel @Inject constructor(
-    mainRepository: MainRepository
+    private val mainRepository: MainRepository
 ) : ViewModel() {
 
-
+    var totalRunTime: LiveData<Long> = mainRepository.getTotalRunDurationInMillis()
+    var totalDistanceRun: LiveData<Int> = mainRepository.getTotalDistanceMTR()
+    var totalCaloriesBurned: LiveData<Int> = mainRepository.getTotalCaloriesBurned()
+    var averageSpeed: LiveData<Float> = mainRepository.getTotalAvgSpeedKMPH()
 
 }
